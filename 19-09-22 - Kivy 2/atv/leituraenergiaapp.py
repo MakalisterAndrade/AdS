331020c6b635b721ad97865a7718e45bfb851ed3
MakalisterAndrade <95728126+MakalisterAndrade@users.noreply.github.com>
2022-09-20 21:18:29 -0400

    atv

---
+++ 19-09-22 - Kivy 2/atv/leituraenergiaapp.py
@@ -0,0 +1,12 @@
+import kivy
+kivy.require('2.1.0')
+
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.config import Config
+
+class TelaPrincipal(GridLayout):
+    pass
