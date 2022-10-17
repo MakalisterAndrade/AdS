5ed1f0af1b9c6f488ef32a9fd8ee9d9cd59b40a1
MakalisterAndrade <95728126+MakalisterAndrade@users.noreply.github.com>
2022-10-17 19:32:55 -0400

    atualizacao

--- Programação II/10-10-22 - Crud e MySQL/model/alunoModel.py
+++ Programação II/10-10-22 - Crud e MySQL/model/alunoModel.py
@@ -12,3 +12,44 @@ class Aluno:
         self.matricula = matricula
         self.cpf = cpf
         self.dataNasc = dataNasc
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def matricula(self):
+        return self._matricula
+
+    @matricula.setter
+    def matrticula(self, matricula):
+        self._matricula = matricula
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self._cpf = cpf
+
+    @property
+    def dataNasc(self):
+        return self._dataNasc
+
+    @dataNasc.setter
+    def dataNasc(self, dataNasc):
+        self._dataNasc = dataNasc
+
